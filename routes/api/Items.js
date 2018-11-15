ab7b5b2ee07801127a5c25d11eb8fa64bdc5be5a
kksrinivasan <srinivasan.kk@terralogic.com>
2018-11-15 14:04:11 +0530

    Node and express Backend is added along with REST Apis.

---
+++ routes/api/Items.js
@@ -0,0 +1,30 @@
+const express = require('express');
+const router = express.Router();
+
+const Items = require('../../models/Items');
+
+router.get('/', (req, res) => {
+  Items.find({}).then(result => {
+    // const temp_result = [{ name: 'srinivasan', date: Date.now() }];
+    return res.json(result);
+  });
+});
+
+router.post('/', (req, res) => {
+  const newItem = new Items({
+    name: req.body.name
+  });
+  newItem.save().then(item => res.json(item));
+});
+
+router.delete('/:id', async (req, res) => {
+  Items.findById({ _id: req.params.id })
+    .then(Item =>
+      Item.remove()
+        .then(() => res.json({ success: true }))
+        .catch(() => res.status(404).json({ success: false }))
+    )
+    .catch(() => res.status(404).json({ success: false }));
+});
+
+module.exports = router;
---
+++ server.js
@@ -0,0 +1,24 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const body_parser = require('body-parser');
+
+const config = require('./config/keys');
+const Items = require('./routes/api/Items');
+
+const app = express();
+//body parser middleware
+app.use(body_parser.json());
+
+mongoose.connect(config.database);
+mongoose.Promise = global.Promise;
+
+mongoose.connection.on('error', err => {
+  console.log('Database error ' + err);
+});
+
+app.use('/api/items', Items);
+
+const port = process.env.PORT || 5000;
+app.listen(port, () => {
+  console.log(`Server is listening to port: ${port}`);
+});
